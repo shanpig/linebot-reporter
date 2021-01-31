7469c81e4c0d645c4a9bb65b4740f4de63860bf2
shanpig <shanpigliao@gmail.com>
2021-01-31 10:48:35 +0800

    linebot reporter for hackmd

---
+++ app.js
@@ -0,0 +1,45 @@
+const linebot = require('linebot');
+const firebase = require('firebase');
+
+const bot = linebot({
+  channelId: process.env.channelId,
+  channelSecret: process.env.channelSecret,
+  channelAccessToken: process.env.channelAccessToken,
+})
+
+const firebaseConfig = {
+  apiKey: process.env.firebaseApiKey,
+  authDomain: process.env.firebaseAuthDomain,
+  databaseURL: process.env.firebaseDBUrl,
+  projectId: process.env.firebaseProjectId,
+  storageBucket: process.env.firebaseStorageBucket,
+  messagingSenderId: process.env.firebaseMessagingSenderId,
+  appId: process.env.firebaseAppId,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+firebase.auth().signInWithEmailAndPassword("shanpigliao@gmail.com", process.env.firebasepwd)
+  .then((user) => {
+    console.log('firebase successfully logged in');
+    database.ref('/test').once('value').then(snap=>{
+      data = snap.val();
+    });
+  })
+  .catch((error) => {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log(errorCode+": "+errorMessage);
+  });
+
+bot.on('message', (e)=>{
+  let message = e.message.text;
+  console.log(message);
+  e.reply(message);
+})
+
+bot.listen('/linewebhook', 3000, ()=>{
+  console.log('準備就緒');
+})
